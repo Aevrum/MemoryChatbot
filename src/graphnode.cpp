555731ee803964094acbf8e99291dc9fa90f9ee7
Aevrum <147383258+Aevrum@users.noreply.github.com>
2023-10-09 10:43:25 +0200

    Add files via upload

---
+++ src/graphnode.cpp
@@ -0,0 +1,60 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// Changed CODE
+    ////
+
+    //delete _chatBot; 
+
+    ////
+    //// EOF Changed CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.emplace_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+{
+    _childEdges.emplace_back(std::move(edge));
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot *chatbot)
+{
+    _chatBot = chatbot;
+    _chatBot->SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(_chatBot);
+    _chatBot = nullptr; // invalidate pointer at source
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
